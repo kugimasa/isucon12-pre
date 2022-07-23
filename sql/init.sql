9f0fa79071bccf037314ab2104a82a5bce7c2619
wata0611 <53475916+wata0611@users.noreply.github.com>
2022-07-23 15:57:37 +0900

    Update init.sql
    visit_history index

--- sql/init.sql
+++ sql/init.sql
@@ -2,3 +2,6 @@ DELETE FROM tenant WHERE id > 100;
 DELETE FROM visit_history WHERE created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
+
+drop index tenant_id_idx on visit_history;
+alter table visit_history add index tenant_competition_created_at_idx (tenant_id,competition_id,created_at);
