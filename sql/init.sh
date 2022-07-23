1daf1386ce78fd784de6d9d136b791d0ecb20088
mtsgi <oq@live.jp>
2022-07-23 13:40:26 +0900

    init.sh 01_create_mysql_database.sql 追加

--- sql/init.sh
+++ sql/init.sh
@@ -9,6 +9,12 @@ ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
 
+# 01_create_mysql_database.sql
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" < 01_create_mysql_database.sql
+
 # MySQLを初期化
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
