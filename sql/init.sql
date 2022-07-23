98ba7bcaa1a85c0af12e966fe41fb22be31b2886
mtsgi <oq@live.jp>
2022-07-23 17:00:40 +0900

    tenant_competition_created_at_idx コメントアウト

--- sql/init.sql
+++ sql/init.sql
@@ -3,4 +3,4 @@ DELETE FROM visit_history WHERE created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
 
-alter table visit_history add index tenant_competition_created_at_idx (tenant_id,competition_id,created_at);
+-- alter table visit_history add index tenant_competition_created_at_idx (tenant_id,competition_id,created_at);
