baa8c2a84a47d6413fc50d7cfc1d1d91d8f4fcc2
mtsgi <oq@live.jp>
2022-07-23 13:34:42 +0900

    mysql再起動順番変更

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,10 @@ echo "[アプリケーションサーバー再起動]"
 sudo systemctl daemon-reload
 sudo systemctl restart isuports.service
 
-echo "[MySQL再起動]"
-
-sudo systemctl restart mysql
-
 echo "[init.sh 実行]"
 
 ./sql/init.sh
+
+echo "[MySQL再起動]"
+
+sudo systemctl restart mysql
